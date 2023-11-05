add9f1674c408f60d12f707dd7ece90f0ade99ce
cindyhu2023 <cindyhu2023@u.northwestern.edu>
2023-11-04 22:50:15 -0500

    restructure

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,6 @@
 import Question from './Question';
 import TestResponse from './TestResponse';
+import TestResponse2 from './TestResponse2';
 import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" exact element={<Question />} />
       <Route path="/test_response" element={<TestResponse />} />
+      <Route path="/test_response2" element={<TestResponse2 />} />
     </Routes>
     </Router>
     
