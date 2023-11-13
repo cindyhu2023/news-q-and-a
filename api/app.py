1e6901968a5050b909f7ca59bf705dbcb63e8c99
cindyhu2023 <cindyhu2023@u.northwestern.edu>
2023-11-13 13:11:42 -0600

    fix util import

--- api/app.py
+++ api/app.py
@@ -9,6 +9,7 @@ from flask_mysqldb import MySQL
 import datetime
 import pytz
 import json
+from .util.utils import remove_duplicate_references
 from openai import OpenAI
 
 load_dotenv()
@@ -141,38 +142,3 @@ def log_response():
 # if __name__ == '__main__':
 #     setup_logging(logging.INFO)
 #     app.run()
-
-####### util functions #######
-
-def remove_duplicate_references(answer, references):
-    answer = answer.replace("[", "|")
-    answer = answer.replace("]", "|")
-    answer_list = answer.split("|")
-    unique_references = {}
-    url_to_index = {}
-    next_index = 1
-    res = ""
-    for i, item in enumerate(answer_list):
-        if i % 2 == 0:
-            res += item
-            continue
-        else:
-            refs = item.replace(" ", "").split(",")
-            curr = []
-            for ref in refs:
-                if ref in curr:
-                    continue
-                elif references[int(ref)] not in url_to_index:
-                    url_to_index[references[int(ref)]] = str(next_index)
-                    unique_references[next_index] = references[int(ref)]
-                    curr.append(str(next_index))
-                    next_index += 1
-                else:
-                    new_ref = url_to_index[references[int(ref)]]
-                    if new_ref not in curr:
-                        curr.append(new_ref)
-            new_item = ",".join(curr)
-            res += "[" + new_item + "]" if new_item else ""
-
-
-    return res, unique_references
